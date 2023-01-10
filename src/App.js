ea3e98c42c348804883902e1d7e63894a0dd9414
Seha Dalay <sehagulnar@gmail.com>
2023-01-10 09:35:37 -0500

    Converted to Responsive Design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //import Product from "./pages/Product";
 //import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+//import Cart from "./pages/Cart";
 //import Register from "./pages/Register";
-//import Home from "./pages/Home";
+import Home from "./pages/Home";
 //import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Cart />;
+  return <Home />;
 };
 
 export default App;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
 `;
@@ -16,16 +17,19 @@ const FilterContainer = styled.div`
 `;
 const Filter= styled.div`
     margin:20px;
-`;
+     ${mobile({ width :"0px 20px", display:"flex", flexDirection:"column" })}
+   `;
 const FilterText= styled.span`
     font-size: 20px;
     font-weight: 600;
     margin-right: 20px;
 `;
 const Select = styled.select`
-    padding:10px;
-    margin-right:20px;
-`;
+    padding: 10px;
+    margin-right: 20px;
+    ${mobile({ margin: "10px 0px" })}
+   `;
+   
 const Option = styled.option`
 `;
 
@@ -37,7 +41,8 @@ export const ProductList = () => {
         <NavBar/>
         <Title>Home Textile</Title>
         <FilterContainer>
-            <Filter><FilterText>Filter Products:</FilterText></Filter>
+            <Filter>
+                <FilterText>Filter Products:</FilterText>
             <Select>
                 <Option disabled selected>
                     Products
@@ -46,6 +51,7 @@ export const ProductList = () => {
                 <Option>Waldorf Dolls</Option>
                 <Option>Doll Clothing</Option>
             </Select>
+            </Filter>
             <Filter><FilterText>Sort Products:</FilterText>
             <Select>
                 <Option selected>
